9bfc685b3b0dd57ecf1e3483a70e5cb1c74b0b66
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-10-10 17:50:42 +0800

    Update RecursiveBinarySearch.h

--- RecursiveBinarySearch.h
+++ RecursiveBinarySearch.h
@@ -1,16 +1,1 @@
-#ifndef RECURSIVEBINARYSEARCH_H
-#define RECURSIVEBINARYSEARCH_H
 
-#include <vector>
-
-
-class RecursiveBinarySearch
-{
-public:
-    
-    bool search(std::vector<int>, int);
-
-    
-};
-
-#endif
